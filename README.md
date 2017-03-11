8ba4375016be097cda0e0607b50c8ed7af9dbed0
tania.stromnes <tania.stromnes@chep.com>
2017-03-11 15:45:38 +0200

    Initial commit of my copy to repo

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
+#Frontend Nanodegree Arcade Game
+
+##Installation
+To Install clone the bibucket repository [Arcade Game](http://23232) into a local folder.
+
+To play the game locally, run the index.htm file in your browser.
+
+##Playing the Game
+The aim of the game is to reach the water without being hit by a bug.
+The bugs will randomly appear in different rows.
+If you reach the water you will clock up a win and the bugs will speed up.
+If you collide with a bug, you scored will drop by 1 and the bugs will slow slightly.
+
+
+
 frontend-nanodegree-arcade-game
 ===============================
+another title
+-------------
 
 Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
 
--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(tileX, tileY, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = tileX * width;
+    this.tileY = tileY;
+    this.speed = speed;
 };
 
 // Update the enemy's position, required method for game
@@ -14,23 +17,134 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += dt * this.speed * gameSpeed;
+    if (this.x > cols * width) {
+        this.x = 0;
+        this.tileY = newTileY();
+    }
+
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.tileY * height - enemyYOffset);
 };
+
+Enemy.prototype.hasCollided = function(player) {
+    var collision = false;
+        if ( this.tileY === player.tileY && Math.abs(this.x - player.tileX * width) < 80) {
+            collision = true;
+        }
+    return collision;
+
+}
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(tileX, tileY) {
+    this.sprite = 'images/char-pink-girl.png';
+    this.tileX = tileX;
+    this.tileY = tileY;
+};
 
+// After each dt check collisions and score
+Player.prototype.update = function(dt) {
+    if ( this.tileY === 0 ) {
+        wins += 1;
+        restart(1.2);
+    }
+
+    for (let enemy of allEnemies) {
+        if (enemy.hasCollided(this)) {
+            wins -= 1;
+            restart(.9);
+            break;
+        }
+    }
+
+    console.log(wins);
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.tileX * width, this.tileY * height - playerYOffset);
+};
+
+Player.prototype.handleInput = function(key) {
+    switch (key) {
+      case 'left':
+        if (this.tileX > 0){
+           this.tileX -= 1;
+        }
+        break;
+      case 'right':
+        if (this.tileX < cols)
+         this.tileX += 1;
+        break;
+      case 'up':
+       if (this.tileY > 0) {
+            this.tileY -= 1;
+       }
+        break;
+      case 'down':
+        if (this.tileY < rows) {
+           this.tileY += 1;
+        }
+        break;
+  }
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var rows = 5,
+    cols = 4,
+    width = 101,
+    height = 83,
+    enemyYOffset = 21,
+    playerYOffset = 10;
+    gameSpeed = 20,
+    wins = 0;
 
+var allEnemies = [];
+var player = new Player(2, 5);
+allEnemies.push(new Enemy(0, 1, 1));
+allEnemies.push(new Enemy(0, 2, 3));
+allEnemies.push(new Enemy(0, 3, 2));
 
+var restart = function(gameSpeedDiff) {
+    player.tileX = newTileX();
+    player.tileY = 5;
+    allEnemies[0].x = 0;
+    allEnemies[0].tileY = newTileY();
+    allEnemies[1].x = 0;
+    allEnemies[1].tileY = newTileY();
+    allEnemies[2].x = 0;
+    allEnemies[2].tileY = newTileY();
+    gameSpeed *= gameSpeedDiff;
+}
+
+var isCollision = function( x, tileY) {
+    var collision = false;
+    for (enemy of allEnemies) {
+        if ( enemy.tileY === player.tileY && Math.abs(enemy.x - player.tileX * width) < 80) {
+            collision = true;
+            break;
+        }
+    }
+    return collision;
+
+}
+
+// Choose a random new row for the enemy
+var newTileY = function() {
+    return Math.floor(Math.random() * 3) + 1;
+}
+
+// Choose a random new column for the player
+var newTileX = function() {
+    return Math.floor(Math.random() * 4);
+}
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
